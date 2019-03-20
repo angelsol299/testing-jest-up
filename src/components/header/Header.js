a0bd7f3bffabc558fd3e7be4d8c87b9cd0dc6b66
angelsol299 <angelsol299@hotmail.com>
2019-03-20 21:07:45 +0100

    first stylings

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../index.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="wrapper">
+        <h1>API App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
